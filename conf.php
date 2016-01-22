425225622f13159a514dc71ee52bb49ef9668b3c
jack sparrow <jpirates1@gmail.com>
2016-01-22 12:25:36 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,15 +1,7 @@
 <?php
 
 	define('SHOPIFY_SHOP', 'dekta.myshopify.com');
-	define('SHOPIFY_APP_API_KEY', '9c7de0e769693595ae558b37d50eabb0');
-	define('SHOPIFY_APP_PASSWORD', 'bb056189db202919c482ace77f564603');
-
-?>
-
-<?php
-
-	//define('SHOPIFY_SHOP', 'dekta.myshopify.com');
-	//define('SHOPIFY_APP_API_KEY', '02d1e41098c763cd903beaa7b80ecab4');
-	//define('SHOPIFY_APP_PASSWORD', 'bb056189db202919c482ace77f564603');
+	define('SHOPIFY_APP_API_KEY', '<YOUR APP API KEY>');
+	define('SHOPIFY_APP_PASSWORD', '<YOUR APP PASSWORD>');
 
 ?>
